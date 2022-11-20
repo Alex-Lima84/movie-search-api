9516eab3ed0573c8f27c042ed039149f9cc94b82
Alexandre Luiz <alexandre.cerutti@live.com>
2022-11-20 10:59:48 -0300

    carousel implementation

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import api from "../../services/api";
 import { Link } from "react-router-dom";
 
@@ -13,9 +13,19 @@ export type MovieType = {
   vote_average?: number;
 };
 
+const delay = 5000;
+
 export default function Home() {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState<boolean>(true);
+  const [index, setIndex] = useState(0);
+  const timeoutRef: any = useRef(null);
+
+  function resetTimeout() {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+  }
 
   useEffect(() => {
     async function loadNowPlayingMovies() {
@@ -29,10 +39,21 @@ export default function Home() {
 
       setMovies(data.results.slice(0, 10));
       setLoading(false);
-      console.log(data.results.slice(0, 10));
     }
     loadNowPlayingMovies();
-  }, []);
+    resetTimeout();
+    timeoutRef.current = setTimeout(
+      () =>
+        setIndex((prevIndex) =>
+          prevIndex === movies.length - 1 ? 0 : prevIndex + 1
+        ),
+      delay
+    );
+
+    return () => {
+      resetTimeout();
+    };
+  }, [index]);
 
   if (loading) {
     return (
@@ -43,20 +64,35 @@ export default function Home() {
   }
 
   return (
-    <div className="container">
-      <div className="movies-list-container">
-        {movies.map((movie: MovieType) => {
-          return (
-            <article key={movie.id}>
+    <div className="slideshow">
+      <div
+        className="slideshowSlider"
+        style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
+      >
+        {movies.map((movie: MovieType, index) => (
+          <div className="slide" key={movie.id}>
+            <div className="movie-container">
               <h2>{movie.title}</h2>
               <img
                 src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
                 alt={movie.title}
               />
               <Link to={`/movie/${movie.id}`}>See more</Link>
-            </article>
-          );
-        })}
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div className="slideshowDots">
+        {movies.map((_, idx) => (
+          <div
+            key={idx}
+            className={`slideshowDot${index === idx ? " active" : ""}`}
+            onClick={() => {
+              setIndex(idx);
+            }}
+          ></div>
+        ))}
       </div>
     </div>
   );
