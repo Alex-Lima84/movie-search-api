c5dfac7a93975f64bc081f11cf9bcb8840876468
Alexandre Luiz <alexandre.cerutti@live.com>
2022-11-29 15:39:37 -0300

    implementations

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,25 @@
 import "./styles.scss";
+import websiteLogo from "../../assets/website-logo2.png";
 
 export default function Footer() {
   return (
     <footer>
-      <h3>Footer</h3>
+      <div className="middle-footer-div">
+        <div className="lower-footer-div">
+          <div className="contact-info-container">
+            <a
+              href="mailto:alexandre.cerutti@live.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <h3>Contact ✉</h3>
+            </a>
+          </div>
+          <div className="logo-container">
+            <img src={websiteLogo} alt="logo"></img>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 }
